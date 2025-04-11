bf00bc54d616f8b35ddf5256d2ec4e36e5c8ed0e
Miguel Caires Montalvão <133923664+MiguelCSenai@users.noreply.github.com>
2025-04-11 07:59:49 -0300

    Update transitions.js

--- Wiki/js/General/transitions.js
+++ Wiki/js/General/transitions.js
@@ -17,7 +17,7 @@ function ativarTransicao() {
                 document.getElementById("overlay").classList.add("fadeOut");
                 setTimeout(() => {
                     
-                    window.location.href = "../../php/Lore/lore.php";
+                    window.location.href = "/Wiki/php/Lore/lore.php";
             
                 }, 680);
     
