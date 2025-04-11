7b7826943ffcd0738e0b4ce8deb590c9a9ebec3c
MiguelCairesDev <miguelcairesdev@gmail.com>
2025-04-11 07:42:46 -0300

    Setup

--- Wiki/js/General/transitions.js
+++ Wiki/js/General/transitions.js
@@ -17,7 +17,7 @@ function ativarTransicao() {
                 document.getElementById("overlay").classList.add("fadeOut");
                 setTimeout(() => {
                     
-                    window.location.href = "/TheKingsWill/Wiki/php/Lore/lore.php";
+                    window.location.href = "./Wiki/php/Lore/lore.php";
             
                 }, 680);
     
