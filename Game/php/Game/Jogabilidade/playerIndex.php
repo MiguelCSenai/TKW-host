5b2bce86caeea0f6f954a01d021f16609b6d17b1
MiguelCairesDev <miguelcairesdev@gmail.com>
2025-04-13 20:19:15 -0300

    Desenvolvimento das mecânicas IOT - 12/04/25

--- Game/php/Game/Jogabilidade/playerIndex.php
+++ Game/php/Game/Jogabilidade/playerIndex.php
@@ -1,10 +1,16 @@
 <?php
+session_start();
 
-    session_start();
+if (isset($_SESSION['classe']) && isset($_SESSION['nome']) && isset($_SESSION['reino'])) {
 
-    if (isset($_SESSION['classe']) && isset($_SESSION['nome']) && isset($_SESSION['reino'])) {
-        
+    include "../../mysqlconecta.php";
 
+    $nome = $_SESSION['nome'];
+    $sql = "SELECT pla_HP, pla_STR, pla_AGI, pla_INT, pla_lvl, pla_xp FROM players WHERE pla_nome = ?";
+    $nome = $_SESSION['nome'];
+    $sql = "SELECT * FROM players WHERE pla_nome = '$nome'";
+    $result = mysqli_query($conexao, $sql);
+    $player = mysqli_fetch_assoc($result);
 ?>
 
 <!DOCTYPE html>
@@ -12,27 +18,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Cadastro de Player</title>
+    <title>Ficha de Personagem</title>
     <link rel="stylesheet" href="../../../css/game/mobile.css">
     <link rel="stylesheet" href="../../../css/general/fonts.css">
     <link rel="stylesheet" href="../../../css/general/attributes.css">
 </head>
 <body>
-    
+
     <div class="container-personagem">
-
-        <div class="img"><?php echo $_SESSION['classe'] ?></div>
-
-        <p class="subtitle bold mediumT"><?php echo $_SESSION['nome'] ?></p>
-
-        <p class="subtitle bold">Prisioneiro do reino <?php echo $_SESSION['reino'] ?></p>  
+        <div class="img"><?php echo $_SESSION['classe']; ?>
+            <div class="level-container"><?php echo $player['pla_lvl']; ?></div>
+        </div>
+        <p class="subtitle bold mediumT"><?php echo $_SESSION['nome']; ?></p>
+        <div class="xp-container">
+            <?php
+                $xp = $player['pla_xp'];
+                $xpMax = 10 * $player['pla_lvl'];
+                $xpPercent = min(100, ($xp / $xpMax) * 100);
+            ?>
+            <div class="xp-bar">
+                <div class="xp-fill" style="width: <?php echo $xpPercent; ?>%;"></div>
+                <div class="xp-text"><?php echo $xp; ?> / <?php echo $xpMax; ?> XP</div>
+            </div>
+        </div>
+        <p class="subtitle bold">Prisioneiro do reino <?php echo $_SESSION['reino']; ?></p>  
 
     </div>
 
+    <div class="stats">
+        <?php
+        $stats = [
+            "pla_HP" => "HP",
+            "pla_STR" => "FOR",
+            "pla_AGI" => "AGI",
+            "pla_INT" => "INT"
+        ];
+
+        foreach ($stats as $key => $atributo) {
+            $valor = $player[$key];
+            $max = 30;
+            $porcentagem = min(100, ($valor / $max) * 100);
+            echo "
+            <div class='stat'>
+                <strong class='subtitle'>{$atributo}:</strong>
+                <div class='stat-bar'>
+                    <span class='stat-num'>{$valor} / {$max}</span>
+                    <div class='stat-fill' style='width: {$porcentagem}%;'></div>
+                </div>
+            </div>
+            ";
+        }
+        ?>
+    </div>
+
 </body>
+</html>
 
-<?php }else{
-
-        header("Location: ../cadastro.php");
-
-} ?>
+<?php 
+} else {
+    header("Location: ../cadastro.php");
+    exit;
+}
+?>
