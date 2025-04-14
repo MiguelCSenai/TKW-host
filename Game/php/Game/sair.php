867937c1e30b6d9a6872f8437beffeff1f1219e6
Miguel Caires <migamesbryt2008@gmail.com>
2025-04-14 17:02:11 -0300

    Correção de bugs - 14/04/2025

--- Game/php/Game/sair.php
+++ Game/php/Game/sair.php
@@ -6,7 +6,7 @@
 
     $ses_id = $_SESSION['ses_id'];
 
-    $query = "UPDATE sessoes SET ses_fim = NOW() WHERE ses_id{$ses_id}";
+    $query = "UPDATE sessoes SET ses_fim = NOW() WHERE ses_id = {$ses_id}";
     mysqli_query($conexao, $query);
 
 
