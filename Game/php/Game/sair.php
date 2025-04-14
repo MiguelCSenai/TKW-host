ae8733ff41f7f3a51a1988fbdc280f079c208d2b
Miguel Caires <migamesbryt2008@gmail.com>
2025-04-14 16:57:06 -0300

    Correção de bugs - 14/04/2025

--- Game/php/Game/sair.php
+++ Game/php/Game/sair.php
@@ -1,6 +1,15 @@
 <?php
 
     session_start();
+
+    include "../mysqlconecta.php";
+
+    $ses_id = $_SESSION['ses_id'];
+
+    $query = "UPDATE sessoes SET ses_fim = NOW() WHERE ses_id{$ses_id}"
+    mysqli_query($conexao, $query);
+
+
     session_unset();
     session_destroy();
 
